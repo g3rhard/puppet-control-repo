d9e2aff6547777630091efea1620e1e2d84228e7
Kiriru Trofimov <kiriru.trofimov@gmail.com>
2019-06-27 16:22:46 +0200

    add site.pp

---
+++ manifests/site.pp
@@ -0,0 +1,14 @@
+ global $PATH
+ Exec {
+     path =>  [
+           '/bin/',
+               '/sbin/',
+                   '/usr/bin/',
+                       '/usr/sbin/',
+                         ],
+ }
+
+ node default {
+     hiera_include('classes')
+ }
+
